603c577ef535c41dc9e93b2937f8fbe0c362ffd3
Jemmy Zhou <jemmy.zhou.s@gmail.com>
2020-05-21 16:20:31 -0700

    added printerbutton file

---
+++ printerbutton.py
@@ -0,0 +1,27 @@
+from PyQt5.QtWidgets import (QWidget, QApplication, QPushButton,
+                             QLabel, QHBoxLayout, QSizePolicy, QFrame)
+from PyQt5.QtGui import QColor
+from PyQt5.QtCore import QPropertyAnimation, pyqtProperty, QRect, Qt
+
+class PrinterButton(QPushButton):
+
+    def __init__(self, text, widget, width=50, height=20, xPos=0, yPos=0, visible=True,
+                 border=None, font_size="12px", color="#000000", background_color="#ffffff",
+                 border_radius="0px"):
+        super().__init__(text, widget)
+        self.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.setVisible(visible)
+
+        self.setStyleSheet("background-color:%s; \
+                            border:%s; \
+                            border-radius:%s; \
+                            font-size:%s; \
+                            color:%s" 
+                            % (background_color, border, border_radius, font_size, color))
+
+        self.move(xPos, yPos)
+        self.setGeometry(QRect(xPos, yPos, width, height))
+
+
+    
+
